3f5ef733a78adaa23b145b093303ff070c69c25e
affjljoo3581 <affjljoo3581@gmail.com>
2020-06-01 07:47:43 +0900

    Upgrade version to 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dist.Distribution().fetch_build_eggs(['Cython'])
 
 setup(
     name='canrevan',
-    version='1.1.0',
+    version='1.1.1',
 
     author='Jungwoo Park',
     author_email='affjljoo3581@gmail.com',
