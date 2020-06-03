f7542339e21ecb2be9e38b64ca953f88fa0099f5
affjljoo3581 <user@.localdomain>
2020-06-03 15:28:24 +0900

    Upgrade version to 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dist.Distribution().fetch_build_eggs(['Cython'])
 
 setup(
     name='canrevan',
-    version='1.1.4',
+    version='1.2.0',
 
     author='Jungwoo Park',
     author_email='affjljoo3581@gmail.com',
