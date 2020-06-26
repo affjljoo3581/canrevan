475c6e3095f448774231131d731384a98bb69c23
affjljoo3581 <affjljoo3581@gmail.com>
2020-06-27 08:29:34 +0900

    Upgrade to 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dist.Distribution().fetch_build_eggs(['Cython'])
 
 setup(
     name='canrevan',
-    version='1.2.1',
+    version='1.3.0',
 
     author='Jungwoo Park',
     author_email='affjljoo3581@gmail.com',
