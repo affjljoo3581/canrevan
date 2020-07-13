bcce87cf040d66559596b90fe4906d43da89abe6
affjljoo3581 <affjljoo3581@gmail.com>
2020-07-13 19:41:41 +0900

    Update version to 2.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dist.Distribution().fetch_build_eggs(['Cython'])
 
 setup(
     name='canrevan',
-    version='2.1.0',
+    version='2.1.1',
 
     author='Jungwoo Park',
     author_email='affjljoo3581@gmail.com',
