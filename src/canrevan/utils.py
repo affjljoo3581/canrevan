4522ee63b00b3398070149bd0cb2dd8511f5835a
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-19 23:42:22 +0900

    Remove unnecessary utilities and implement new ones

--- src/canrevan/utils.py
+++ src/canrevan/utils.py
@@ -1,24 +1,5 @@
-import os
-import random
-import string
-import asyncio
-import functools
 from datetime import datetime, timedelta
-from typing import List, Iterable, Any, Callable
-
-
-def notifiable(func):
-    @functools.wraps(func)
-    async def wrapper(*args, callback: Callable = None, **kwargs):
-        try:
-            result = await func(*args, **kwargs)
-        finally:
-            # If callback is given, then call callback function to notify that
-            # the given function is finished.
-            if callback is not None:
-                callback()
-        return result
-    return wrapper
+from typing import List
 
 
 def drange(start: str, end: str, step: int = 1) -> List[str]:
@@ -30,60 +11,17 @@ def drange(start: str, end: str, step: int = 1) -> List[str]:
             for d in range(iters + 1)]
 
 
-def split_list(seq: List, chunks: int) -> List[List]:
-    chunk_list = []
-    chunk_size = len(seq) // chunks
+def korean_character_ratio(text: str, ignore_whitespace: bool = True) -> float:
+    if ignore_whitespace:
+        text = ''.join(text.split())
 
-    for i in range(chunks - 1):
-        chunk_list.append(seq[i * chunk_size:(i + 1) * chunk_size])
-    chunk_list.append(seq[(chunks - 1) * chunk_size:])
-
-    return chunk_list
+    korean_characters = len([
+        c for c in text if ord('가') <= ord(c) <= ord('힣')])
+    return korean_characters / len(text)
 
 
-def random_filename(parent: str) -> str:
-    return os.path.join(parent, ''.join(random.choices(
-        string.digits + string.ascii_lowercase, k=16)))
-
-
-def random_filenames(parent: str, n: int) -> List[str]:
-    return [random_filename(parent) for _ in range(n)]
-
-
-class Context(object):
-    def __init__(self, max_tasks: int):
-        self.max_tasks = max_tasks
-
-    async def run(self, aws: Iterable) -> Any:
-        tasks, results, errors = set(), [], 0
-        for a in aws:
-            if len(tasks) >= self.max_tasks:
-                # If pool is full, wait for the first-completed task.
-                done, tasks = await asyncio.wait(
-                    tasks, return_when=asyncio.FIRST_COMPLETED)
-
-                # Update results or exceptions.
-                for d in done:
-                    try:
-                        r = d.result()
-                        if r is not None:
-                            results.append(r)
-                    except Exception:
-                        errors += 1
-
-            # Add new task and put into the pool.
-            tasks.add(asyncio.ensure_future(a))
-
-        # After adding all tasks to the pool, wait for the rest tasks.
-        done, _ = await asyncio.wait(tasks)
-
-        # Update results or exceptions.
-        for d in done:
-            try:
-                r = d.result()
-                if r is not None:
-                    results.append(r)
-            except Exception:
-                errors += 1
-
-        return results, errors
+def is_normal_character(c: str) -> bool:
+    return (ord('0') <= ord(c) <= ord('9')
+            or ord('a') <= ord(c) <= ord('z')
+            or ord('A') <= ord(c) <= ord('Z')
+            or ord('가') <= ord(c) <= ord('힣'))
