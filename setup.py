732fe4951fb0b0ea491b5aad816c33759f6831f4
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-20 08:32:12 +0900

    Upgrade the package version to 3.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dist.Distribution().fetch_build_eggs(['Cython'])
 
 setup(
     name='canrevan',
-    version='3.0.0',
+    version='3.0.1',
 
     author='Jungwoo Park',
     author_email='affjljoo3581@gmail.com',
