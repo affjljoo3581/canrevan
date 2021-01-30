314355a9e82ab8a663e2fc1abe5c7a46ffe8e97c
affjljoo3581 <affjljoo3581@gmail.com>
2021-01-30 13:33:11 +0900

    Upgrade the version to 4.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dist.Distribution().fetch_build_eggs(["Cython"])
 
 setup(
     name="canrevan",
-    version="3.1.1",
+    version="4.0.0",
     author="Jungwoo Park",
     author_email="affjljoo3581@gmail.com",
     description="Simple Naver News Crawler",
